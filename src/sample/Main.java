0e038b6591215c3db5b7af8d909927fb338360ca
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-04-21 17:09:13 +0200

    Add pictures

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.CvType;
+import org.opencv.core.Scalar;
 
 public class Main extends Application {
 
+
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        /*
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.show();*/
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+
+        System.out.println("Welcome to OpenCV " + Core.VERSION);
+        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
+        System.out.println("OpenCV Mat: " + m);
+        Mat mr1 = m.row(1);
+        mr1.setTo(new Scalar(1));
+        Mat mc5 = m.col(5);
+        mc5.setTo(new Scalar(5));
+        System.out.println("OpenCV Mat data:\n" + m.dump());
+
     }
 }
